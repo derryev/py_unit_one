a95e752136b65ed7321a437ffaddcf322b5b687a
derryev <derryev@ssfs.org>
2023-09-15 14:17:19 -0400

    messing around with turtle

---
+++ turtlepractice.py
@@ -0,0 +1,22 @@
+import turtle
+colA = input("enter the first shape's color name")
+colB = input("enter the second shape's color name")
+
+turtle.fillcolor(colA)
+turtle.begin_fill()
+turtle.forward(200)
+turtle.left(45)
+turtle.forward(200)
+turtle.left(45)
+turtle.forward(200)
+turtle.left(135)
+turtle.forward(490)
+turtle.end_fill()
+
+turtle.penup
+turtle.fillcolor(colB)
+turtle.begin_fill()
+
+
+
+turtle.exitonclick()
