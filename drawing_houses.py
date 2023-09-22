c6aa673804514a72d8b0529f485fac2946d7303a
derryev <derryev@ssfs.org>
2023-09-22 13:41:49 -0400

    drew simple house

--- drawing_houses.py
+++ drawing_houses.py
@@ -7,20 +7,9 @@ def draw_house(square_side_length, color):
         turtle.forward(square_side_length)
         turtle.right(90)
     turtle.left(60)
-    square_side_length = square_side_length*.708
     for x in range(2):
         turtle.forward(square_side_length)
-        turtle.right(90)
-
-def draw_house(square_side_length, color):
-    for x in range(4):
-        turtle.forward(square_side_length)
-        turtle.right(90)
-    turtle.left(60)
-    square_side_length = square_side_length*.708
-    for x in range(2):
-        turtle.forward(square_side_length)
-        turtle.right(90)
+        turtle.right(120)
 
 
 draw_house(300,  "black")
