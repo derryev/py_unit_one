b773dec914e33a8969a4c4efd17df259e7d8f7f3
derryev <derryev@ssfs.org>
2023-09-22 13:44:47 -0400

    house now colored in

--- drawing_houses.py
+++ drawing_houses.py
@@ -3,6 +3,8 @@
 import turtle
 
 def draw_house(square_side_length, color):
+    turtle.color(color)
+    turtle.begin_fill()
     for x in range(4):
         turtle.forward(square_side_length)
         turtle.right(90)
@@ -10,9 +12,8 @@ def draw_house(square_side_length, color):
     for x in range(2):
         turtle.forward(square_side_length)
         turtle.right(120)
+    turtle.end_fill()
 
-
-draw_house(300,  "black")
-
+draw_house(300,"black")
 
 turtle.exitonclick()
