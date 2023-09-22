44bb610184fc5ad746542e5e482d4e06e2e1126c
derryev <derryev@ssfs.org>
2023-09-22 13:32:33 -0400

    Completed assignment_one.py code (draws four octagons in different colors).

--- assignment_one.py
+++ assignment_one.py
@@ -1,32 +1,42 @@
 # Eva D
 # 9/21/23
-# Demonstrating Properties of functions
+# Demonstrating properties of functions by drawing 4 octagons with turtle
 
-# remember to use comments!
+#imports turtle program which will be used to draw octagons
 import turtle
 
-#draws an octagon and takes parameter to make octagon a certain color when function is called
+#draws an octagon, takes parameter to make octagon a certain color when function is called
 def draw_octagon(color, side_length):
     turtle.color(color)
     turtle.begin_fill()
+    #loop to draw octagon
     for x in range(8):
         turtle.forward(side_length)
         turtle.left(45)
     turtle.end_fill()
 
-#moves turtle to new x and y coordinates without writing on the paper
+#moves turtle to certain coordinates without writing on the paper, takes parameters for x and y coordinates
 def move_pen(x, y):
     turtle.penup()
     turtle.goto(x, y)
     turtle.pendown()
 
+#moves pen to top left corner
 move_pen(-300, 100)
+#draws pink octagon
 draw_octagon("pink")
+#moves pen to top right corner
 move_pen(200, 100)
+#draws teal octagon
 draw_octagon("teal")
+#moves pen to bottom left corner
 move_pen(-300, -350)
+#draws violet octagon
 draw_octagon("violet")
+#moves pen to bottom right corner
 move_pen(200, -350)
+#draws sky blue octagon
 draw_octagon("skyblue")
 
+#keeps turtle graphics window from closing before desired
 turtle.exitonclick()
